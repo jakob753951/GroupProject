e9b7bdb7e424e8fad2a13d9928d3ff81b449b2b9
migru2 <migru23@student.sdu.dk>
2023-11-27 13:45:52 +0100

    Created an interactive bubblepĺot, life expectancy vs GDP per capita

--- main.R
+++ main.R
@@ -8,5 +8,88 @@ data |>
   select(Life.expectancy)
 
 
-# Test Git
-# Daniel nervt extrem
+library(ggplot2)
+library(gganimate)
+library(dplyr)
+library(countrycode)
+library(plotly)
+library(ggrepel)
+
+# Read the CSV file and add Continent information
+data <- read.csv("Life_Expectancy_Data.csv", header = TRUE) %>%
+  mutate(Continent = countrycode(Country, "country.name", "continent"))
+
+# Exclude data from the year 2015
+data <- data %>% filter(Year != 2015)
+
+# Create ggplot
+p_animate <- ggplot(
+  data,
+  aes(x = GDP, y = Life.expectancy, size = Population, colour = Continent, label = Country)
+) +
+  geom_point(show.legend = TRUE, alpha = 0.4) +  
+  scale_color_manual(values = c("Asia" = "red", "Europe" = "blue", "Africa" = "green", "Americas" = "yellow", "Oceania" = "black"),
+                     name = "Continent") +  
+  scale_size_continuous(range = c(2, 10), name = "Population") +  
+  scale_x_log10() +  
+  labs(x = "GDP per Capita", y = "Life Expectancy")+  
+  transition_states(Life.expectancy) +
+  enter_fade() +
+  ease_aes('linear', nframes = 700) +
+  theme(
+    legend.position = "bottom",
+    legend.box = "horizontal",
+    legend.key.size = unit(0.2, "cm")
+  ) +  
+  guides(
+    size = guide_legend(title.position = "top", title.vjust = 0.3),
+    color = guide_legend(title.position = "top", title.vjust = 0.3)
+  ) +
+  ggtitle("Year: {frame_time}") +
+  shadow_mark(alpha = 0.6, size = 0.4) +
+  enter_fade() +
+  exit_fade() +
+  geom_text_repel(aes(label = ifelse(frame_time == max(frame_time), as.character(Country), "")))
+
+library(plotly)
+
+# Prepare data
+# data <- read.csv("Life_Expectancy_Data.csv", header = TRUE) %>%
+#  mutate(Continent = countrycode(Country, "country.name", "continent"))
+
+# Create Plotly graph
+p_animate_plotly <- plot_ly(
+  data,
+  x = ~GDP, y = ~Life.expectancy,
+  size = ~Population, color = ~Continent,
+  text = ~paste("Country: ", Country, ", Population: ", Population),
+  frame = ~Year,
+  marker = list(sizemode = "diameter", line = list(width = 1)),  # Adjust the line width
+  type = "scatter",
+  mode = "markers"
+) %>%
+  layout(
+    xaxis = list(type = "log", title = "GDP per Capita"),
+    yaxis = list(title = "Life Expectancy"),
+    showlegend = TRUE,
+    legend = list(
+      x = 1,
+      y = 1,
+      traceorder = 'normal',
+      font = list(
+        family = 'sans-serif',
+        size = 12,
+        color = '#000'
+      ),
+      bgcolor = '#E2E2E2',
+      bordercolor = '#FFFFFF',
+      borderwidth = 2
+    )
+  ) %>%
+  animation_opts(frame = 1500, redraw = TRUE)  # Adjust the frame duration (in milliseconds)
+
+# Save or display
+# save the plotly object to an HTML file
+htmlwidgets::saveWidget(p_animate_plotly, file = "interactive_plot-Lifeexp.html")
+# or show it in the viewer
+p_animate_plotly
