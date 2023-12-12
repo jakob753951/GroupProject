cc2c2626c86f84023393b57075917e4be0aca6d8
Daniel Sakarli <daniel.sakarli@fau.de>
2023-12-12 13:32:04 +0100

    Added colors and commented out unnecessary code

--- main.R
+++ main.R
@@ -6,6 +6,7 @@ library(gganimate)
 library(gifski)
 library(transformr)
 library(plotly)
+
 data <- read.csv("Life_Expectancy_Data.csv", header = TRUE)
 data$Continent <- countrycode(data$Country, "country.name", "continent")
 
@@ -14,10 +15,8 @@ data |>
   slice_min(Year) |>
   select(Life.expectancy)
 
-
 averages_hepatitis <- data.frame(Country = character(), Average = numeric(), stringsAsFactors = FALSE)
 averages_under_five_deaths <- data.frame(Country = character(), Average = numeric(), stringsAsFactors = FALSE)
-
 
 # Get unique countries in the data frame
 unique_countries <- unique(data$Country)
@@ -35,7 +34,6 @@ for (country in unique_countries) {
   # Append the result to the averages data frame
   averages_hepatitis <- rbind(averages_hepatitis, data.frame(Country = country, "Hepatitis B Average" = hepatitis_average))
   averages_under_five_deaths <- rbind(averages_under_five_deaths, data.frame(Country = country, "Under Five Deaths Average" = under_five_death_average))
-  
 }
 
 data <- left_join(data, averages_hepatitis, by = "Country")
@@ -70,11 +68,6 @@ predicted_values_asia <- predict(linear_model_asia)
 predicted_values_asia[predicted_values_asia < 0] <- 0
 rmse_asia <- sqrt(mean(resid(linear_model_asia)^2))
 
-scatter_plot_asia <- plot_ly(data_model_asia, x = ~data_model_asia$Under.Five.Deaths.Average, y = ~data_model_asia$Hepatitis.B.Average, text = data_model_asia$Country, mode = "markers", type = "scatter", name = "Asian countries") %>%
-  layout(title = "Interactive Scatter Plot", xaxis = list(title = "Death under age of 5"), yaxis = list(title = "Hepatitis B Cases")) %>%
-  add_lines(x = ~data_model_asia$Under.Five.Deaths.Average, y = ~predicted_values_asia, line = list(color = 'red'), name = "Linear regression", hoverinfo = "text", text = rep(paste("RMSE: ", rmse_asia), length(predicted_values_asia)))
-scatter_plot_asia
-
 # Africa
 data_model_africa   <- subset(data_africa) #, Country) # != "Russian Federation")
 linear_model_africa <- lm(data_model_africa$Hepatitis.B.Average ~ data_model_africa$Under.Five.Deaths.Average, data = data_model_africa)
@@ -82,11 +75,6 @@ predicted_values_africa <- predict(linear_model_africa)
 # Set values below 0 to 0 --> still some error here: truncates values but now there is a bend in the line which should be straight
 predicted_values_africa[predicted_values_africa < 0] <- 0
 rmse_africa <- sqrt(mean(resid(linear_model_africa)^2))
-
-scatter_plot_africa <- plot_ly(data_model_africa, x = ~data_model_africa$Under.Five.Deaths.Average, y = ~data_model_africa$Hepatitis.B.Average, text = data_model_africa$Country, mode = "markers", type = "scatter", name = "African countries") %>%
-  layout(title = "Interactive Scatter Plot", xaxis = list(title = "Death under age of 5"), yaxis = list(title = "Hepatitis B Cases")) %>%
-  add_lines(x = ~data_model_africa$Under.Five.Deaths.Average, y = ~predicted_values_africa, line = list(color = 'red'), name = "Linear regression", hoverinfo = "text", text = rep(paste("RMSE: ", rmse_africa), length(predicted_values_africa)))
-scatter_plot_africa
 
 # Americas
 data_model_americas   <- subset(data_americas)
@@ -96,11 +84,6 @@ predicted_values_americas <- predict(linear_model_america)
 predicted_values_americas[predicted_values_americas < 0] <- 0
 rmse_america <- sqrt(mean(resid(linear_model_america)^2))
 
-scatter_plot_americas <- plot_ly(data_model_americas, x = ~data_model_americas$Under.Five.Deaths.Average, y = ~data_model_americas$Hepatitis.B.Average, text = data_model_americas$Country, mode = "markers", type = "scatter", name = "American countries") %>%
-  layout(title = "Interactive Scatter Plot", xaxis = list(title = "Death under age of 5"), yaxis = list(title = "Hepatitis B Cases")) %>%
-  add_lines(x = ~data_model_americas$Under.Five.Deaths.Average, y = ~predicted_values_americas, line = list(color = 'red'), name = "Linear regression", hoverinfo = "text", text = rep(paste("RMSE: ", rmse_america), length(predicted_values_americas)))
-scatter_plot_americas
-
 # Oceania
 data_model_oceania   <- subset(data_oceania)
 linear_model_oceania <- lm(data_model_oceania$Hepatitis.B.Average ~ data_model_oceania$Under.Five.Deaths.Average, data = data_model_oceania)
@@ -109,60 +92,30 @@ predicted_values_oceania <- predict(linear_model_oceania)
 predicted_values_oceania[predicted_values_oceania < 0] <- 0
 rmse_oceania <- sqrt(mean(resid(linear_model_oceania)^2))
 
-scatter_plot_oceania <- plot_ly(data_model_oceania, x = ~data_model_oceania$Under.Five.Deaths.Average, y = ~data_model_oceania$Hepatitis.B.Average, text = data_model_oceania$Country, mode = "markers", type = "scatter", name = "Oceanian countries") %>%
-  layout(title = "Interactive Scatter Plot", xaxis = list(title = "Death under age of 5"), yaxis = list(title = "Hepatitis B Cases")) %>%
-  add_lines(x = ~data_model_oceania$Under.Five.Deaths.Average, y = ~predicted_values_oceania, line = list(color = 'red'), name = "Linear regression", hoverinfo = "text", text = rep(paste("RMSE: ", rmse_oceania), length(predicted_values_oceania)))
-scatter_plot_oceania
-
-## Subplots
-combined_plot <- subplot(scatter_plot_europe, scatter_plot_asia, scatter_plot_africa, scatter_plot_americas, scatter_plot_oceania)
-combined_plot
-
-
-
-
-
-
 ###############################################################################
 # Combine Subplots in one plot
+
+new_color <- c("#1f78b4", "#33a02c", "#ff7f00", "#e31a1c", "#6a3d9a")
+
 data_models <- list(africa = data_model_africa, americas = data_model_americas, asia = data_model_asia, europe = data_model_europe, oceania = data_model_oceania)
 
-scatter_plot <- plot_ly(data_models$africa, x = ~data_models$africa$Under.Five.Deaths,  y = ~data_models$africa$Hepatitis.B.Average, text = data_models$africa$Country, mode = "markers", type = "scatter", name = "African countries") %>%
-  add_trace(x = ~data_models$americas$Under.Five.Deaths,  y = ~data_models$americas$Hepatitis.B.Average, text = data_models$americas$Country, mode = "markers", type = "scatter", name = "American countries") %>%
-  add_trace(x = ~data_models$asia$Under.Five.Deaths,  y = ~data_models$asia$Hepatitis.B.Average, text = data_models$asia$Country, mode = "markers", type = "scatter", name = "Asian countries") %>%
-  add_trace(x = ~data_models$europe$Under.Five.Deaths,  y = ~data_models$europe$Hepatitis.B.Average, text = data_models$europe$Country, mode = "markers", type = "scatter", name = "European countries") %>%
-  add_trace(x = ~data_models$oceania$Under.Five.Deaths,  y = ~data_models$oceania$Hepatitis.B.Average, text = data_models$oceania$Country, mode = "markers", type = "scatter", name = "Oceanian countries")%>%
+scatter_plot <- plot_ly(data_models$africa, x = ~data_models$africa$Under.Five.Deaths,  y = ~data_models$africa$Hepatitis.B.Average, text = data_models$africa$Country, mode = "markers", type = "scatter", name = "African countries", colors = new_color[1]) %>%
+  add_trace(x = ~data_models$americas$Under.Five.Deaths,  y = ~data_models$americas$Hepatitis.B.Average, text = data_models$americas$Country, mode = "markers", type = "scatter", name = "American countries", colors = new_color[2]) %>%
+  add_trace(x = ~data_models$asia$Under.Five.Deaths,  y = ~data_models$asia$Hepatitis.B.Average, text = data_models$asia$Country, mode = "markers", type = "scatter", name = "Asian countries", colors = new_color[3]) %>%
+  add_trace(x = ~data_models$europe$Under.Five.Deaths,  y = ~data_models$europe$Hepatitis.B.Average, text = data_models$europe$Country, mode = "markers", type = "scatter", name = "European countries", colors = new_color[4]) %>%
+  add_trace(x = ~data_models$oceania$Under.Five.Deaths,  y = ~data_models$oceania$Hepatitis.B.Average, text = data_models$oceania$Country, mode = "markers", type = "scatter", name = "Oceanian countries", colors = new_color[5])%>%
   layout(title = "Child death relation towards Hepathitis B immunization coverage", xaxis = list(title = "Death under age of 5"), yaxis = list(title = "Hepatitis B immunization coverage in percent"))
 scatter_plot
 
   
-linear_model <- lm(data_models$Hepatitis.B.Average ~ data_models$Under.Five.Deaths.Average, data = data_models)
-predicted_values <- predict(linear_models)
+#linear_model <- lm(data_models$Hepatitis.B.Average ~ data_models$Under.Five.Deaths.Average, data = data_models)
+#predicted_values <- predict(linear_models)
 # Set values below 0 to 0 --> still some error here: truncates values but now there is a bend in the line which should be straight
-predicted_values[predicted_values < 0] <- 0
-rmse_prediction <- sqrt(mean(resid(linear_model)^2))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#predicted_values[predicted_values < 0] <- 0
+#rmse_prediction <- sqrt(mean(resid(linear_model)^2))
 
 ###############################################################################
-
+# DO NOT PLOT THIS, THIS IS JUST FOR CHECKING THE QUALITY OF THE LINEAR REGRESSION
 ## Checking the grade of the linear regression
 # Europe
 # Residual Analysis: should be close to zero and evenly distributed
