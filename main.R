b68ddf5ffcf4ec2dfe784f1d7acaf58552df3c10
Daniel Sakarli <daniel.sakarli@fau.de>
2023-12-23 13:23:45 +0100

    Preprocessing of population numbers

--- main.R
+++ main.R
@@ -25,6 +25,20 @@ data <- read.csv("Life_Expectancy_Data.csv", header = TRUE) %>%
 # Exclude data from the year 2015
 data <- data %>% filter(Year != 2015)
 
+# Preprocess population data
+# India
+data <- data %>% 
+  mutate(Population = ifelse(Country == "India" & (Year == 2004 | Year == 2005 | Year == 2006 | Year == 2007 | Year == 2014), Population / 10, Population))
+data <- data %>% 
+  mutate(Population = ifelse(Country == "India" & Year == 2010, Population * 10, Population))
+data <- data %>% 
+  mutate(Population = ifelse(Country == "India", Population * 10, Population))
+# Indonesia:
+data <- data %>% 
+  mutate(Population = ifelse(Country == "Indonesia" & (Year == 2000 | Year == 2003 | Year == 2005 | Year == 2006 | Year == 2009 | Year == 2011 | Year == 2013), Population * 10, Population))
+data <- data %>% 
+  mutate(Population = ifelse(Country == "Indonesia" & (Year == 2001 | Year == 2002), Population * 100, Population))
+
 # Define new colors
 new_color <- c("#1f78b4", "#33a02c", "#ff7f00", "#e31a1c", "#6a3d9a")
 
